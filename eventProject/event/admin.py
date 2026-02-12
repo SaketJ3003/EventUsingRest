37e159d7dac5f58fbbe32c83cb2f3fafc90d1b9d
SaketJ3003 <saketjangir3003@gmail.com>
2026-02-12 18:02:15 +0530

    Created Login with JWT auth and made a Image models to add extra Images and views count of particular event.

--- eventProject/event/admin.py
+++ eventProject/event/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Category,Event
+from .models import Category, Event, EventTag, EventImages
+
 # Register your models here.
 admin.site.register(Event)
 admin.site.register(Category)
+admin.site.register(EventTag)
+admin.site.register(EventImages)
